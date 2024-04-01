f0dc289aa82fee28258a4b7b50ec1b770961fc04
viet-cuong <viet-cuong@system-exe.com.vn>
2024-04-01 14:04:37 +0700

    Build-up src: add guards, interceptors, pipes

--- demo-microservices/src/app.module.ts
+++ demo-microservices/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MathController } from './controllers/math.controller';
 
 @Module({
 	imports: [],
-	controllers: [AppController, MathController],
+	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {}
