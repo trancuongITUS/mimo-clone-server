acd24842d0436b48788b06efbdd2d8c5e2a7ad8c
Tran Quang Phat <phattran.2002.work@gmail.com>
2024-05-20 22:10:56 +0700

    Unit test course controller and E2E testing /courses

--- api-gateway/test/app.e2e-spec.ts
+++ api-gateway/test/app.e2e-spec.ts
@@ -4,21 +4,23 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+	let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+	it('/api/v1 (GET)', () => {
+		return request(app.getHttpServer()).get('/api/v1').expect(200);
+	});
+
+	it('/courses (GET)', () => {
+		return request(app.getHttpServer()).get('/courses').expect(200);
+	});
 });
+
